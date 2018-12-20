b6708d5f5688062252c4ede1ab96c5a02bfa6170
mesfin mengesha <mtsegaye2-c@cityu.edu.hk>
2018-12-21 06:58:15 +0800

    Modified GLM summary file

--- src/lib/getGLMSummary.R
+++ src/lib/getGLMSummary.R
@@ -1,31 +1,16 @@
-# For summerizing  differnt models in to one table
+# For summerizing  and comparing differnt models 
 
-# method 1 : Model selection approach summerizes and compare  models based on their AICc values 
-
-# Inputes : Models names,as Model1, Model2, ..... 
-
-library(MuMIn)
-msAICc <- model.sel(Model1,Model2)
-model.sel(msAICc, rank = AIC, rank.args = alist(k = log(nobs(x))))
-
-
-
-
-# Method 2 ,gives summary of coeffcients, doesnot compare 
+ # Model selection approach summerizes and compare  models based on their AICc values 
 
 # Inputes : Models names,as Model1, Model2, ..... 
 
-library(broom)
+getGLMSummary <- function(rank=rank, ...) {
+  library(MuMIn)
+  selectionSummary <- model.sel(rank=AICc, ...)
+  return(selectionSummary)
+}
 
-models <- list(Model1,Model2)
-names(models) <- paste0("MODEL", 1:2)
 
-all_coefs <- plyr::ldply(models, tidy, .id = "model")
-head(all_coefs)
 
-library(dplyr)
-library(tidyr)
-results <- all_coefs %>% select(-(std.error:p.value)) %>%
-  spread(term, estimate)
 
 
