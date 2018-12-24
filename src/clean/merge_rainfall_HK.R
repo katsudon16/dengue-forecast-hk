396a52cc66279cc48287953d4e91975b741492c7
KathleenSucipto <sucipto.kathleen@gmail.com>
2018-12-24 16:18:20 +0800

    Fixed HK (HKCD)

--- src/clean/merge_rainfall_HK.R
+++ src/clean/merge_rainfall_HK.R
@@ -1,12 +1,12 @@
 # Merge HK rainfall
-# WCH+HV, Cape, Quarry Bay
+# HV, Cape, Quarry Bay
 # Creates an excel file containing the merge result
 
 rm(list=ls(all=TRUE))
 if (!require(openxlsx)) install.packages("openxlsx")
 
 # daily data
-excelFile <- "d:/workspace/dengue-forecast-hk/dat/climate/HKCD_test.xlsx"
+excelFile <- "../../dat/climate/HKCD_test.xlsx"
 daily <- read.xlsx("../../dat/climate/HK_rainfall.xlsx",
                    sheet="Daily",
                    startRow=1, colNames=TRUE, detectDates=TRUE)
@@ -15,10 +15,10 @@ daily$Quarry.Bay <- as.numeric(gsub("[^.0-9]", "", daily$Quarry.Bay))
 dailydf <- data.frame()
 
 for (i in 1:dim(daily)[1]) {
-  totalValid <- ifelse(is.na(daily$`WCH+HV`[i]), 0, 2) +
+  totalValid <- ifelse(is.na(daily$HV[i]), 0, 1) +
                 ifelse(is.na(daily$Cape[i]), 0, 1) +
                 ifelse(is.na(daily$Quarry.Bay[i]), 0, 1)
-  sumValid <- ifelse(is.na(daily$`WCH+HV`[i]), 0, 2 * daily$`WCH+HV`[i]) +
+  sumValid <- ifelse(is.na(daily$HV[i]), 0, daily$HV[i]) +
               ifelse(is.na(daily$Cape[i]), 0, daily$Cape[i]) +
               ifelse(is.na(daily$Quarry.Bay[i]), 0, daily$Quarry.Bay[i])
   rainfall <- sumValid / totalValid
@@ -30,20 +30,20 @@ write.xlsx(dailydf, excelFile, append=TRUE)
 
 
 # monthly data
-excelFile <- "d:/workspace/dengue-forecast-hk/dat/climate/HKCD_test.xlsx"
+excelFile <- "../../dat/climate/HKCD_test.xlsx"
 monthly <- read.xlsx("../../dat/climate/HK_rainfall.xlsx",
                    sheet="Monthly",
                    startRow=1, colNames=TRUE, detectDates=TRUE)
-monthly$`WCH+HV` <- as.numeric(gsub("[^.0-9]", "", monthly$`WCH+HV`))
+monthly$HV <- as.numeric(gsub("[^.0-9]", "", monthly$HV))
 monthly$Cape <- as.numeric(gsub("[^.0-9]", "", monthly$Cape))
 monthly$Quarry.Bay <- as.numeric(gsub("[^.0-9]", "", monthly$Quarry.Bay))
 monthlydf <- data.frame()
 
 for (i in 1:dim(monthly)[1]) {
-  totalValid <- ifelse(is.na(monthly$`WCH+HV`[i]), 0, 2) +
+  totalValid <- ifelse(is.na(monthly$HV[i]), 0, 1) +
     ifelse(is.na(monthly$Cape[i]), 0, 1) +
     ifelse(is.na(monthly$Quarry.Bay[i]), 0, 1)
-  sumValid <- ifelse(is.na(monthly$`WCH+HV`[i]), 0, 2 * monthly$`WCH+HV`[i]) +
+  sumValid <- ifelse(is.na(monthly$HV[i]), 0, monthly$HV[i]) +
     ifelse(is.na(monthly$Cape[i]), 0, monthly$Cape[i]) +
     ifelse(is.na(monthly$Quarry.Bay[i]), 0, monthly$Quarry.Bay[i])
   rainfall <- sumValid / totalValid
